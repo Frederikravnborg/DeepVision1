3c95ecbe4d69e680cf5f0a85d96a31b68a820750
Frederikravnborg <frederik@ravnborg.fr>
2024-10-28 13:38:55 +0100

    Update project2DataLoaders.py

--- project2DataLoaders.py
+++ project2DataLoaders.py
@@ -1,22 +1,10 @@
-import os
 import numpy as np
 import glob
 import PIL.Image as Image
-
-# pip install torchsummary
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.datasets as datasets
 from torch.utils.data import DataLoader, random_split
 import torchvision.transforms as transforms
-from torchvision import models
-from torchsummary import summary
-import torch.optim as optim
-from time import time
-
 import matplotlib.pyplot as plt
-from IPython.display import clear_output
 import random
 
 
