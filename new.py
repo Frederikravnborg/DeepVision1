9572661f31b74c60b313a5e87c9c3ca10ea07166
dani8377 <duendaniel@live.dk>
2024-11-17 12:33:01 +0100

    f

--- new.py
+++ new.py
@@ -29,7 +29,7 @@ TRAINING_DATA_FILE = os.path.join(DATASET_DIR, 'training_data.pkl')  # Combined 
 # Training Parameters
 NUM_CLASSES = 2  # 1 object class + 1 background
 BATCH_SIZE = 32
-NUM_EPOCHS = 10
+NUM_EPOCHS = 1
 LEARNING_RATE = 0.001
 VALIDATION_SPLIT = 0.2
 RANDOM_SEED = 42
